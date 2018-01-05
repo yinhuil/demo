9704848618ca639a0a547977a87c0b97069346a3
yinhui <yinhui@bupt.edu.cn>
2018-01-05 08:42:18 +0800

    xiugai

--- src/leetCode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/leetCode/LongestSubstringWithoutRepeatingCharacters.java
@@ -11,7 +11,22 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
     Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is
     a subsequence and not a substring.*/
-    public int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring(String s) {
+        int[] dp=new int[256];
+        for(int i=0;i<dp.length;i++){
+            dp[i]=-1;
+        }
+        int start=0,result=0;
+        for(int i=0;i<s.length();i++){
+            start=Math.max(dp[s.charAt(i)]+1,start);
+            dp[s.charAt(i)]=i;
+            result=Math.max(result,i-start+1);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        String str="abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
     }
 }
