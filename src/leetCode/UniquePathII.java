83c233079fdb6f4e7eb7e7d31001186c24169e00
jiang <yinhui@bupt.edu.cn>
2018-01-08 14:29:51 +0800

    增加几个代码

--- src/leetCode/UniquePathII.java
+++ src/leetCode/UniquePathII.java
@@ -37,4 +37,19 @@ public class UniquePathII {
          }
          return dp[m - 1][n - 1];
      }
+//第二種方法。回去看看
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1)
+                    dp[j] = 0;
+                else if (j > 0)
+                    dp[j] += dp[j - 1];
+            }
+        }
+        return dp[width - 1];
+    }
 }
