907348b8d7d901241e1dde83508c6f74af7ab708
yinhui <yinhui@bupt.edu.cn>
2018-01-27 09:40:34 +0800

    xiugai

--- src/leetCode/AddOneRowtoTree.java
+++ src/leetCode/AddOneRowtoTree.java
@@ -82,16 +82,16 @@ public class AddOneRowtoTree {
         help(root.right,v,d,lens+1);
     }
 //    第二种，采用非递归的方法；
-    public TreeNode addOneRow(TreeNode root, int v, int d) {
-        if(d==0||d==1){
-            TreeNode temp=new TreeNode(v);
-            temp.left=(d==1?root:null);
-            temp.right=(d==0?root:null);
-            return temp;
+    public TreeNode addOneRow1(TreeNode root, int v, int d) {
+        if (d == 0 || d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = d == 1 ? root : null;
+            newroot.right = d == 0 ? root : null;
+            return newroot;
         }
-        if(root!=null && d>=2){
-            root.left=addOneRow(root.left,v,d > 2 ? d - 1 : 1);
-            root.right=addOneRow(root.right,v,d>2?d-1:0);
+        if (root != null && d >= 2) {
+            root.left  = addOneRow(root.left,  v, d > 2 ? d - 1 : 1);
+            root.right = addOneRow(root.right, v, d > 2 ? d - 1 : 0);
         }
         return root;
     }
